8336253e080f94a778ed90b82d6360ff429e3f4f
Abdur Rahman <119003720+siyamar@users.noreply.github.com>
2025-09-10 07:59:48 +0600

    added and fixed purchase api

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -2,9 +2,21 @@ const Purchase = require("../models/Purchase");
 
 exports.buyCourse = async (req, res) => {
   const { courseId, amount } = req.body;
+  // Check duplicate course
+  const existingCourse = await Purchase.findOne({
+    userId: req.user.id,
+    courseId: courseId
+  });
+  if (existingCourse) {
+    return res.json({
+      message: "Already purchased",
+      course: existingCourse
+    });
+  }
+
   const purchase = new Purchase({ userId: req.user.id, courseId, amount });
   await purchase.save();
-  res.json({ message: "Course purchased", purchase });
+  res.json({status: "Success", message: "Course purchased", purchase });
 };
 
 exports.myPurchases = async (req, res) => {
